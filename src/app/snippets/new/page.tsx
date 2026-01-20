b24aa71c13694c911a204a08701ccb4cc5daae8f
Cody James Reed <cody_j_reed@proton.me>
2026-01-20 16:50:47 -0500

    add form submission handling with server-action and redirect

--- src/app/snippets/new/page.tsx
+++ src/app/snippets/new/page.tsx
@@ -1,7 +1,29 @@
-
+import { db } from "@/db"
+import { redirect } from "next/navigation";
 
 export default function SnippetCreatePage() {
-    return <form>
+    async function createSnippet(formData: FormData) {
+        // needs to be a server action
+        'use server';
+
+        // retrieve and validate formData
+        const title = formData.get('title') as string
+        const code = formData.get('code') as string
+
+        // create new snippet
+        const snippet = await db.snippet.create({
+            data: {
+                title,
+                code
+            }
+        })
+
+        console.log("snippet", snippet)
+
+        redirect("/")
+    }
+
+    return <form action={createSnippet}>
         <h3 className="font-bold m-3">Create a Snippet</h3>
         <div className="flex flex-col gap-4">
             <div className="flex gap-4">
