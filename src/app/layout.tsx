6f25c4c499b490371d2ab044202f320d4ac13f5e
Cody James Reed <cody_j_reed@proton.me>
2026-01-20 16:34:05 -0500

    adjust layout max-width; add basic form structure to ~./new/page.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,9 +25,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased `}
       >
-        {children}
+        <div className="container mx-auto px-12 max-w-xxl">
+          {children}
+        </div>
       </body>
     </html>
   );
--- src/app/snippets/new/page.tsx
+++ src/app/snippets/new/page.tsx
@@ -1,5 +1,30 @@
+
+
 export default function SnippetCreatePage() {
-    return <div>
-        Create a Snippet
-    </div>
+    return <form>
+        <h3 className="font-bold m-3">Create a Snippet</h3>
+        <div className="flex flex-col gap-4">
+            <div className="flex gap-4">
+                <label className="w-12" htmlFor="title">Title</label>
+                <input
+                    className="border rounded p-2 w-full"
+                    name="title"
+                    id="title"
+                />
+            </div>
+
+            <div className="flex gap-4">
+                <label className="w-12" htmlFor="code">Code</label>
+                <input
+                    className="border rounded p-2 w-full"
+                    name="code"
+                    id="code"
+                    type="textarea"
+                />
+            </div>
+
+            <button type="submit" className="border rounded p-2 bg-blue-200">Create</button>
+
+        </div>
+    </form>
 }
