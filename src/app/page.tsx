7a59fa9fed52bfb6bfe105ddea18f178cc3d2a4c
Cody James Reed <cody_j_reed@proton.me>
2026-01-22 10:39:15 -0500

    add header, functionality, and styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,32 @@
 import { db } from "@/db";
+import Link from "next/link";
 
 export default async function Home() {
   const snippets = await db.snippet.findMany()
 
-  const renderedSnippets =  snippets.map(snippet => {
-    const {id, title} = snippet
+  const renderedSnippets = snippets.map(snippet => {
+    const { id, title } = snippet
 
     return (
-      <div key={id}>
-        {title}
-      </div>
+      <Link
+        href={`/snippets/${id}`}
+        key={id}
+        className="flex justify-between items-center p-2 border rounded"
+      >
+        <div>{title}</div>
+        <div>View</div>
+      </Link>
     )
   })
   return (
     <div>
-      {renderedSnippets}
+      <div className="flex justify-between py-2 items-center">
+        <h1 className="text-xl font-bold">Snippets</h1>
+        <Link href="/snippets/new" className="border p-2 rounded">New</Link>
+      </div>
+      <div className="flex flex-col gap-2">
+        {renderedSnippets}
+      </div>
     </div>
   );
 }
