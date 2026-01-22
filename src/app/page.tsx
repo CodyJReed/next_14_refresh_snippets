7849591db2fcd251d839b6b1b7b291bf968cf18e
Cody James Reed <cody_j_reed@proton.me>
2026-01-21 21:23:32 -0500

    add dynamic snippet-detail page; fetch data; handle return

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,20 @@
-export default function Home() {
+import { db } from "@/db";
+
+export default async function Home() {
+  const snippets = await db.snippet.findMany()
+
+  const renderedSnippets =  snippets.map(snippet => {
+    const {id, title} = snippet
+
+    return (
+      <div key={id}>
+        {title}
+      </div>
+    )
+  })
   return (
     <div>
-      Home Page
+      {renderedSnippets}
     </div>
   );
 }
