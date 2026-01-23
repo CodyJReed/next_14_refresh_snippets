1c62be2b48548adc33b561a282c82395e90c0bd0
Cody J Reed <creed@pop-os.lan>
2026-01-23 09:37:04 -0500

    import editSnippet; bind to event; test

--- src/components/snippet-edit-form.tsx
+++ src/components/snippet-edit-form.tsx
@@ -1,5 +1,7 @@
 'use client'
 
+import * as actions from "@/actions"
+
 import type { Snippet } from "@prisma/client"
 import { Editor } from "@monaco-editor/react"
 import { useState } from "react"
@@ -15,6 +17,9 @@ export default function SnippetEditForm({ snippet }: ComponentProps) {
         setCode(value)
     }
 
+    // Add in 'bind' server action to update snippet record
+    const editSnippetAction = actions.editSnippet.bind(null, snippet.id, code)
+
     return <div>
         <Editor
             height="40vh"
@@ -22,9 +27,12 @@ export default function SnippetEditForm({ snippet }: ComponentProps) {
             language="javascript"
             defaultValue={snippet.code}
             options={{
-                minimap: {enabled: false}
+                minimap: { enabled: false }
             }}
             onChange={handleEditorChange}
         />
+        <form action={editSnippetAction}>
+            <button type="submit" className="p-2 border rounded">Save</button>
+        </form>
     </div>
 }
