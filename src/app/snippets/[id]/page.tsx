4873a22bbb1feb740165f3afd26c23f27f0d42fb
CodyJReed <cody_j_reed@proton.me>
2026-01-23 09:46:00 -0500

    add handle deletion of snippet(s)

--- src/app/snippets/[id]/page.tsx
+++ src/app/snippets/[id]/page.tsx
@@ -1,6 +1,7 @@
 import { db } from "@/db"
 import { notFound } from "next/navigation"
 import Link from "next/link"
+import * as actions from '@/actions'
 
 interface ComponentProps {
     params: {
@@ -19,13 +20,17 @@ export default async function SnippetDetail(props: ComponentProps) {
 
     if (!snippet) return notFound()
 
+    const deleteSnippetAction = actions.deleteSnippet.bind(null, snippet.id)
+
     return <div>
         <div className="flex m-4 justify-between items-center">
             <h1 className="text-xl font-bold">{snippet.title}</h1>
 
             <div className="flex gap-4">
                 <Link href={`/snippets/${snippet.id}/edit`} className="p-2 border rounded">Edit</Link>
+                <form action={deleteSnippetAction}>
                 <button className="p-2 border rounded">Delete</button>
+                </form>
             </div>
         </div>
         <pre className="p-3 border rounded bg-gray-200 border-x-gray-200">
