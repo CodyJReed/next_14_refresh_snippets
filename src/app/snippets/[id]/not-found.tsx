7a0c26457a59580b0155efa279c8d2b352c801e3
Cody James Reed <cody_j_reed@proton.me>
2026-01-27 16:03:14 -0500

    fix build error; set ./ to force dynamic rendering

--- src/app/snippets/[id]/not-found.tsx
+++ src/app/snippets/[id]/not-found.tsx
@@ -1,7 +1,7 @@
 export default function NotFound() {
     return <div className="">
         <h1 className="text-xl font-bold">
-            Sorry, but we could't find that particular snippet.
+            Sorry, but we couldt find that particular snippet.
         </h1>
     </div>
 }
